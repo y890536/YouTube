b3e0678c0c74b478ebf21e5d63c3a1840334ef3b
y890536 <y890536@gmail.com>
2020-03-24 19:24:54 +0800

    title

---
+++ main.js
@@ -0,0 +1,46 @@
+var channelName = 'TechGuyWeb';
+var vidWidth = 300;
+var vidHeight = 200;
+var vidResults = 10;
+
+$(document).ready(function() {
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels", {
+            part: 'contentDetails',
+            forUsername: channelName,
+            key: 'AIzaSyAFNRcBYd3ODoZJ1OF40Kkv3D3JA0TKV8s'
+        },
+        function(data) {
+            $.each(data.items, function(i, item) {
+                console.log(item);
+                pid = item.contentDetails.relatedPlaylists.uploads;
+                getVids(pid);
+            })
+        }
+    );
+
+    function getVids(pid) {
+        $.get(
+            "https://www.googleapis.com/youtube/v3/playlistItems", {
+                part: 'snippet',
+                maxResults: vidResults,
+                playlistId: pid,
+                key: 'AIzaSyAFNRcBYd3ODoZJ1OF40Kkv3D3JA0TKV8s'
+            },
+            function(data) {
+                $.each(data.items, function(i, item) {
+                    var output;
+                    console.log(item);
+                    videoTitle = item.snippet.title;
+                    videoId = item.snippet.resourceId.videoId;
+
+
+                    output = '<li><iframe height="' + vidHeight + '" width="' + vidWidth + '" src=\"//www.youtube.com/embed/' + videoId + '\">' + videoTitle + '</iframe></li>'
+
+                    //Append to results listStyleTpe
+                    $('#results').append(output);
+                })
+            }
+        );
+    }
+});
