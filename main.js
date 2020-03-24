61d0c8d32f683497faf397ed8c5312a37438224e
y890536 <y890536@gmail.com>
2020-03-24 19:31:40 +0800

    no message

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
                     videoId = item.snippet.resourceId.videoId;
 
 
-                    output = '<li><iframe height="' + vidHeight + '" width="' + vidWidth + '" src=\"//www.youtube.com/embed/' + videoId + '\"></iframe></li>'
+                    output = '<h1>test1</h1><br><li><iframe height="' + vidHeight + '" width="' + vidWidth + '" src=\"//www.youtube.com/embed/' + videoId + '\"></iframe></li>'
 
                     //Append to results listStyleTpe
                     $('#results').append(output);
